b11aa84a139724a9428774236a510f6d5ae26404
haenanim121@gmail.com <haenanim121@gmail.com>
2023-10-10 11:25:35 +0900

    feat : Demo페이지

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Demo from './pages/Demo';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<h1>asdf</h1>}></Route>
+        <Route path="/Demo" element={<Demo></Demo>}></Route>
+      </Routes>
+      <Link to="/Demo">데모 페이지 표시</Link>
+    </div>
+  );
 }
 
 export default App;
