91977649e991b8383361ca9b98bad1b4106d9a27
haenanim121@gmail.com <haenanim121@gmail.com>
2023-10-11 12:23:21 +0900

    fix : 로고 클릭 시 메인페이지 이동

--- src/components/ETHeader.js
+++ src/components/ETHeader.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Logo from '../img/ET_Logo.png';
 import '../styles/ETHeader.css';
+import { Link } from 'react-router-dom';
 
 function ETHeader() {
   return (
     <header className="ETMain-header">
-      <img className="ETMain-Logo" src={Logo}></img>
+      <Link to="/ETMain">
+        <img className="ETMain-Logo" src={Logo}></img>
+      </Link>
       <div className="input-area">
         <span class="material-symbols-outlined search-icon">search</span>
         <input className="search" type="text"></input>
