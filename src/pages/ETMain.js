b2de07e31af676c1704d4f179fcd5d7c1fe78378
haenanim121@gmail.com <haenanim121@gmail.com>
2023-10-11 11:54:15 +0900

    feat : 방 목록 기능 추가 및 CSS수정

--- src/pages/ETMain.js
+++ src/pages/ETMain.js
@@ -12,6 +12,24 @@ import cat3 from '../img/cat3.jpg';
 function ETMain() {
   // 프롭으로 넘겨주기 위한 배열
   const images = [cat, cat2, cat3];
+  const roomList = [
+    {
+      UID: 0,
+      PlaceID: 0,
+      HostID: 0,
+      RoomName: '점심 먹으러 가실분',
+      MaxPeople: 4,
+      Day: new Date('2023-10-09 12:00:00'),
+    },
+    {
+      UID: 1,
+      PlaceID: 1,
+      HostID: 1,
+      RoomName: '우동 가실분',
+      MaxPeople: 4,
+      Day: new Date('2023-10-09 12:30:00'),
+    },
+  ];
   return (
     <div className="ETMain">
       <ETHeader />
@@ -19,10 +37,28 @@ function ETMain() {
       <ImageCarousel images={images}></ImageCarousel>
       <hr />
       <div className="now-room">
-        <h3>현재 방 목록</h3>
-        <div>room1</div>
-        <div>room2</div>
+        <h3 className="room-list jejugothic">현재 방 목록</h3>
+        {roomList.map((e, i) => {
+          return (
+            <div className="room-item-container">
+              <div className="room-info-container">
+                <div className="room-name">{e.RoomName}</div>
+                <div className="place-id">{e.PlaceID}번 지역</div>
+                <div className="room-date">{e.Day.toLocaleString()};</div>
+              </div>
+              <div className="join-btn-container">
+                <div className="square-button-container">
+                  <div className="max-people">0 / {e.MaxPeople}</div>
+                </div>
+                <button className="square-button">같이 먹기</button>
+              </div>
+            </div>
+          );
+        })}
       </div>
+      <hr />
+      <hr />
+      <hr />
       <ETNav />
     </div>
   );
